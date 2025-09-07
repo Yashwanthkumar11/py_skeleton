44985eb0d0be440c00243fcef8e7c59d801f6211
Yashwanthkumar11 <yb91374@gmail.com>
2025-09-07 23:47:52 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from pyutils_lib.services.StatTimer import StatTimer
-from pyutils_lib.services.config_manager import ConfigManager
+from pyutils_lib.services.StatTimer import StatTimer                   # type: ignore
+from pyutils_lib.services.config_manager import ConfigManager          # type: ignore
 
-ConfigManager().define_setting("setting_name",False,'xyz',"string","This is a normal non-secret setting") 
+ConfigManager().define_setting("setting_name",False,'setting_value',"string","This is a normal non-secret setting") 
 ConfigManager().define_setting("secret_setting_name",True,None,"string","This is a secret setting") 
 
 ConfigManager().load_configuration()
